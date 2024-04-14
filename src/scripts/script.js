7413c0ba18e1c0d0da5926c176e9c73bd17cf7b0
Apolo Trasmonte <apolgaming18@gmail.com>
2024-04-14 21:07:34 +0800

    Updated the dashboard page, added sidebar menu, footer, and designs. Added images to the image source. Added javascripts.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -22,7 +22,52 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var overlay = document.getElementById("overlay");
+    var menuToggle = document.getElementById("menu-toggle");
+    var content = document.getElementById("content");
+    var topnav = document.querySelector(".topnav");
+    var footer = document.querySelector(".footer");
+    var menuIcon = document.getElementById("menu-icon");
+    var closeIcon = document.getElementById("close-icon");
 
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+    closeIcon.classList.toggle("active");
+    content.classList.toggle("active");
+    topnav.classList.toggle("active"); // Toggle active class for topnav
+    footer.classList.toggle("active"); // Toggle active class for footer
+    document.body.classList.toggle("sidebar-active"); // Toggle class for body
+
+    if (sidebar.classList.contains("active")) {
+        menuToggle.setAttribute("onclick", "closeSidebar()");
+    } else {
+        menuToggle.setAttribute("onclick", "toggleSidebar()");
+    }
+}
+
+function closeSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var overlay = document.getElementById("overlay");
+    var menuToggle = document.getElementById("menu-toggle");
+    var content = document.getElementById("content");
+    var topnav = document.querySelector(".topnav");
+    var footer = document.querySelector(".footer");
+    var menuIcon = document.getElementById("menu-icon");
+    var closeIcon = document.getElementById("close-icon");
+
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+    menuIcon.classList.remove("active");
+    closeIcon.classList.remove("active");
+    content.classList.remove("active");
+    topnav.classList.remove("active"); // Remove active class for topnav
+    footer.classList.remove("active"); // Remove active class for footer
+    document.body.classList.remove("sidebar-active"); // Remove class for body
+    menuToggle.setAttribute("onclick", "toggleSidebar()");
+}
 
 
 
