9f5e394efa8aa7267a3148e89ab7bac7a998b73e
Biella Mariscotes <135019439+biellamariscotes@users.noreply.github.com>
2024-05-30 17:05:43 +0800

    ci: modified semantic version (#64)

--- release-config.js
+++ release-config.js
@@ -1,14 +1,11 @@
 const config = {
-    branches: ['development'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      ["@semantic-release/git", {
-        "assets": ["dist/*.js", "dist/*.js.map"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
-      '@semantic-release/github'
-    ]
-  };
-  
-  module.exports = config;
+  branches: ["development"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
